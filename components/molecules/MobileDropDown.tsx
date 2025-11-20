97558498c4456eeca17d7e3722be65bdb549492b
Daniel Praise <praisedaniel979@gmail.com>
2025-11-20 21:34:07 +0100

    more issues fixed

--- components/molecules/MobileDropDown.tsx
+++ components/molecules/MobileDropDown.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { ChevronDown, ChevronUp } from "lucide-react";
 import type { Category, Product } from "@/types/models";
-import { MobileItem } from "@/components/organisms/CategorySection"
+// import { MobileItem } from "@/components/organisms/CategorySection"
 
 export default function MobileDropdown({ root }: { root: Category[] }) {
   const [show, setShow] = useState(false);
@@ -16,13 +16,13 @@ export default function MobileDropdown({ root }: { root: Category[] }) {
         {show ? <ChevronUp className="h-5 w-5" /> : <ChevronDown className="h-5 w-5" />}
       </button>
 
-      {show && (
+      {/* {show && (
         <ul className="absolute mt-3 w-full max-h-[300px] overflow-y-auto rounded-xl border border-gray-200 bg-white px-3 py-3 shadow-md z-10 space-y-1">
           {root.map((cat) => (
             <MobileItem key={cat.id} cat={cat} depth={0} />
           ))}
         </ul>
-      )}
+      )} */}
     </>
   );
 }
